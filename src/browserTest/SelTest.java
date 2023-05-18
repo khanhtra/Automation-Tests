2b78a1b4d25b20a1466b5b4d3d15715e545cf0b2
Your Name <your@email.com>
2023-05-18 17:24:09 -0500

    Amazon test added

--- src/browserTest/SelTest.java
+++ src/browserTest/SelTest.java
@@ -5,14 +5,16 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.support.ui.Select;
 import org.junit.Assert;
+import java.util.concurrent.TimeUnit;
 
 
 
 
 public class SelTest {
-	  public static void main(String[] args) {
+	  public static void main(String[] args) throws InterruptedException {
+		  /*
 		  	//Google youtube
 	        WebDriver driver = new ChromeDriver();
 	        driver.get("https://www.google.com");
@@ -27,7 +29,20 @@ public class SelTest {
 	        String expectedURL = "https://www.youtube.com/";
 	        String actualURL = driver2.getCurrentUrl();
 	        Assert.assertEquals(actualURL, expectedURL);
-	        
-	        
+	        */
+		  
+	        //Open Amazon and click on a product
+	        WebDriver driver3 = new ChromeDriver();
+	        driver3.get("https://www.amazon.com/");
+	        driver3.manage().window().maximize();
+	        WebElement search = driver3.findElement(By.id("twotabsearchtextbox"));
+	        search.sendKeys("PS4");
+	        search.sendKeys(Keys.RETURN);
+	        driver3.findElement(By.linkText("Sony PlayStation 4 Pro w/ Accessories, 1TB HDD, CUH-7215B - Jet Black (Renewed)")).click();
+	        driver3.findElement(By.id("add-to-cart-button")).click();
+	        Thread.sleep(2000);
+	        driver3.findElement(By.id("attachSiNoCoverage")).click();
+	        Thread.sleep(2000);
+	        driver3.findElement(By.id("attach-sidesheet-checkout-button")).click();
 	    }
 	}
